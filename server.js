e394a0fc9c94b15ad3f6d71448caf2163f0ee854
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-04-26 12:28:17 -0400

    working on my routes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const db = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,4 +10,35 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//middleware that connects front-end files
 app.use(express.static('public'));
+
+
+//HTML Routes
+// GET Route for startup page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
+
+//API Routes
+// GET Route for notes db
+app.get('/api/notes', (req, res) => res.json(db));
+
+//**********************
+// POST Request for notes db
+app.post('/api/notes', (req, res) => {
+    // Inform the client that their POST request was received
+    res.json(`${req.method} request received to add a review`);
+  
+    // Log our request to the terminal
+    console.info(`${req.method} request received to add a review`);
+});
+//*******************************
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
