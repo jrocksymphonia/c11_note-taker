cba59abc4e5b116ea6f1cd9cc33b94e642478931
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-05-30 21:46:30 -0400

    save what I have

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static('public'));
 // GET Route for startup page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+); 
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
