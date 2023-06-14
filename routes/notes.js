be52c6b531e2d8237729db3027ba359ca44c4524
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-06-14 16:00:42 -0400

    edited the get api route

--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,22 @@
-
+const notes = require('express').Router();
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
 
 
 
 
 //API Routes
 // GET Route for notes db
-app.get('/api/notes', (req, res) => res.json(db));
+notes.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+})
 
 //**********************
 // POST Request for notes db
-app.post('/api/notes', (req, res) => {
+notes.post('/api/notes', (req, res) => {
     // Inform the client that their POST request was received
     res.json(`${req.method} request received to add a review`);
   
